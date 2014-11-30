a8e3e8b9d819952dc09e92bac8a1033714e6a953
Tony Messias <tonyzrp@gmail.com>
2014-11-30 19:24:08 -0300

    adding styles and user model with migrations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+    has_secure_password
+
+    validates :username, presence: true, uniqueness: { case_sensitive: false }, 
+                         length: { in: 4..12 },
+                         format: { with: /\A[a-z][a-z0-9]*\z/, message: 'can only contain lowercase and numbers' }
+
+    validates :password, presence: true, length: { in: 4..8 }, allow_nil: true
+    validates :password_confirmation, presence: true, length: { in: 4..8 }, allow_nil: true
+end
