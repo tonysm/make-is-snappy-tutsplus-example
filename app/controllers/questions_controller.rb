5ccda452637cc813d7fc2f7d806166b596ea94f5
Tony Messias <tonyzrp@gmail.com>
2014-12-01 00:47:27 -0300

    listing question on question index and paginating it with will_paginate

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,7 @@ class QuestionsController < ApplicationController
 
   def index
       @question = Question.new
+      @questions = Question.unsolved(params)
   end
 
   def create
@@ -12,6 +13,7 @@ class QuestionsController < ApplicationController
           flash[:success] = "Your queston has been posted!"
           redirect_to root_url
       else
+          @questions = Question.unsolved(params)
           render 'index'
       end
   end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,11 @@
 class Question < ActiveRecord::Base
+  self.per_page = 3
   belongs_to :user
 
   validates :body, presence: true, length: { in: 10..255 }
   validates :solved, inclusion: { in: [true, false] }
+
+  def self.unsolved(params)
+      where(:solved => false).paginate(:page => params[:page]).order('created_at DESC')
+  end
 end
