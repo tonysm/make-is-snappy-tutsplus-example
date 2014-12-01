3f7d3f521d82ef1424c81df5d7736bafaa0fc4ae
Tony Messias <tonyzrp@gmail.com>
2014-12-01 11:02:42 -0300

    updating questions and showing when it is solved

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,5 +1,5 @@
 class QuestionsController < ApplicationController
-  before_filter :auth, only: [:create, :your_questions]
+  before_filter :auth, only: [:create, :your_questions, :edit, :update]
 
   def index
       @question = Question.new
@@ -26,7 +26,26 @@ class QuestionsController < ApplicationController
       @questions = current_user.your_questions(params)
   end
 
+  def edit
+      @question = current_user.questions.find(params[:id])
+  end
+
+  def update
+      @question = current_user.questions.find(params[:id])
+
+      if @question.update_attributes(question_edit_params)
+          flash[:success] = 'Your question has been updated'
+          redirect_to @question
+      else
+          render 'edit'
+      end
+  end
+
   private
+    def question_edit_params
+        params.require(:question).permit(:body, :solved)
+    end
+
     def question_params
         params.require(:question).permit(:body)
     end
